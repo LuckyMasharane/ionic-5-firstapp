86973e8e64d622d1cf3a26cddda1c8c95c33e003
LuckyMasharane <lmasharane@gmail.com>
2020-09-21 09:30:22 +0200

    1 file modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: '', component: LoginComponent }
